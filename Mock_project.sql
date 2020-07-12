fbaede8d26821fd99856cf13d0fbdf3aad3e0689
Beckythatcher07 <36010325+Beckythatcher07@users.noreply.github.com>
2020-07-12 11:57:43 +0100

    Add files via upload

--- Mock_project.sql
+++ Mock_project.sql
@@ -1,8 +1,8 @@
 use mock_project;
 DROP TABLE Retail_Store;
-Select * from Retail_Store;
+Select * from Asos;
 
--- All products in database listed in Product_Masterlist table
+-- Table 1: All products in database listed in Product_Masterlist table
 create table Product_Masterlist(
 Product_Name VARCHAR(75),
 Product_Category VARCHAR(50),
@@ -43,7 +43,7 @@ VALUES
 ( 'Nutriplenish Leave-In Conditioner', 'Conditioner', 'Straight', '1A', 'Aveda'),
 ( 'Matcha + Apple Replenishing Superfood Shampoo', 'Shampoo','Wavy', '2A', 'Briogeo');
 
--- Products that can be bought in Superdrug listed here
+-- Table 2: Products that can be bought in Superdrug listed here
 create table Superdrug(
 Product_Name VARCHAR(75),
 Product_Price DECIMAL(4,2),
@@ -51,7 +51,7 @@ Product_Hair_Texture VARCHAR(10),
 Product_Hair_Type VARCHAR(2)
 );
 
--- Products that can be bought in Boots listed here
+-- Table 3: Products that can be bought in Boots listed here
 create table Boots(
 Product_Name VARCHAR(75),
 Product_Price DECIMAL(4,2),
@@ -59,7 +59,7 @@ Product_Hair_Texture VARCHAR(10),
 Product_Hair_Type VARCHAR(2)
 );
 
--- Products that can be bought on Asos listed here
+-- Table 4: Products that can be bought on Asos listed here
 create table Asos(
 Product_Name VARCHAR(75),
 Product_Price DECIMAL(4,2),
@@ -67,7 +67,7 @@ Product_Hair_Texture VARCHAR(10),
 Product_Hair_Type VARCHAR(2)
 );
 
--- Ethical questions about products found in this table
+-- Table 5: Ethical questions about products found in this table
 create table Ethics(
 Product_Name VARCHAR(75),
 Product_Brand VARCHAR(50),
@@ -76,7 +76,7 @@ Vegan VARCHAR(3),
 Natural_Ingredients VARCHAR(3)
 );
 
--- Chemicals found in products shown in this table
+-- Table 6: Chemicals found in products shown in this table
 create table Chemicals(
 Product_Name VARCHAR(75),
 Parabens VARCHAR(3),
@@ -84,7 +84,7 @@ Sulphates VARCHAR(3),
 Silicones VARCHAR(3)
 );
 
--- Product Ratings (out of 5 stars) found in this table
+-- Table 7: Product Ratings (out of 5 stars) found in this table
 create table Rating(
 Product_Name VARCHAR(75),
 Rating INTEGER
@@ -121,10 +121,22 @@ VALUES
 ('Nutriplenish Leave-In Conditioner',2),
 ('Matcha + Apple Replenishing Superfood Shampoo',3);
 
--- Where to buy all the products found here. List of products and if they are found in boots/superdrug/asos.
+-- Table 8: Where to buy all the products found here. List of products and if they are found in boots/superdrug/asos.
 create table Retail_Store(
 Product_Name VARCHAR(75),
 Product_ID INTEGER,
 Product_Brand VARCHAR(50),
 Retail_Store VARCHAR(10)
 );
+
+-- Using a subquery to find the product name and product price of products that can be bought on Asos and are Vegan.
+Select Product_Name, Product_Price
+FROM Asos
+WHERE Product_Name IN (
+SELECT Product_Name
+FROM Ethics
+WHERE Vegan='Yes'
+)
+ORDER BY Product_Price DESC;
+
+-- Create a stored function.
